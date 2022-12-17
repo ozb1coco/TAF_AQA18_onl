7d71335d55f0c4c6aed8a15425d2441b2ad77799
Ozb1coco <ozb1coco2013@mail.ru>
2022-12-17 13:55:50 +0300

    pageObject refactor

--- src/test/java/tests/LoginTest2.java
+++ src/test/java/tests/LoginTest2.java
@@ -2,13 +2,12 @@ package tests;
 
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.LoginPage;
 
-public class LoginTest extends BaseTest {
+public class LoginTest2 extends BaseTest {
 
     @Test
     public void loginTest() {
@@ -35,14 +34,5 @@ public class LoginTest extends BaseTest {
                 .getErrorTextElement().getText(),
                 "Email/Login or Password is incorrect. Please try again."
         );
-    }
-
-    @Test
-    public void addProjectTest() {
-        userStep.loginSuccessful(ReadProperties.username(), ReadProperties.password());
-        projectSteps.addProject("WP_01");
-
-        Assert.assertEquals(driver.findElement(By.className("page_title")).getText(),
-                "WP_01");
     }
 }
